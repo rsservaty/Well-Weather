836bb8ac070d14d775618b4e0fb34b0afc91cd8a
rsservaty <rs@servaty.de>
2026-06-05 00:39:49 +0200

    Wetter App: app.js aktualisiert

--- app.js
+++ app.js
@@ -186,10 +186,12 @@ function initMap() {
         zoomControl: true,
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-        maxZoom: 19,
-    }).addTo(map);
+    const attr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>';
+    const layerVoyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', { attribution: attr, maxZoom: 19 });
+    const layerLight   = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',           { attribution: attr, maxZoom: 19 });
+    const layerDark    = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',            { attribution: attr, maxZoom: 19 });
+    layerVoyager.addTo(map);
+    L.control.layers({ 'Voyager': layerVoyager, 'Hell': layerLight, 'Dunkel': layerDark }, {}, { position: 'topright', collapsed: true }).addTo(map);
     L.control.scale({ imperial: false, position: 'bottomleft' }).addTo(map);
 
     // Klick-Handler
