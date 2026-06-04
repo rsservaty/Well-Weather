531ee1f8732862a4bad9ce4aa769c5489106889a
rsservaty <rs@servaty.de>
2026-06-04 21:58:28 +0200

    Wetter App: service-worker.js aktualisiert

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Strategie: Static Assets cached, API-Calls live
    ===================================================== */
 
-const CACHE = 'wetter-europa-v59';
+const CACHE = 'wetter-europa-v60';
 
 const STATIC = [
     './',
